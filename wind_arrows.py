79b4e52edab2b9465a51c9e22acaf18e703471f3
Jon Seddon <jon.seddon@metoffice.gov.uk>
2020-06-08 18:28:59 +0100

    Zoomed in on the Atlantic crossing

--- wind_arrows.py
+++ wind_arrows.py
@@ -9,6 +9,7 @@ ffmpeg -framerate 5 -i plots/img%06d.png -c:v libx264 -r 30
     -pix_fmt yuv420p wind_arrows.mp4
 """
 import cartopy.crs as ccrs
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 import gpxpy
 import iris
 import iris.plot as iplt
@@ -37,7 +38,7 @@ def main():
 
     # Get just October for a reduced subset
     october = hourly[hourly.time.dt.strftime('%Y%m%d').between('20131001', 
-                                                               '20131031')]
+                                                               '20131107')]
 
     # chose every nth wind point in the netCDF
     nth = 8
@@ -61,20 +62,35 @@ def main():
         v_atl_2deg = v_atl[0, ::nth, ::nth]
         v_atl_2deg.convert_units('knot')
         # Plot data
-        plt.figure(figsize=(12, 25))
+        plt.figure(figsize=(12, 12))
         iplt.quiver(u_atl_2deg, v_atl_2deg)
         iplt.contour(mslp_atl[0], 20,
                          linewidths=0.5, colors='black', linestyles='-')
         ax = plt.gca()
-        ax.set_extent((-70.0, 10.0, -90.0, 60.0), crs=crs_latlon)
+        # ax.set_extent((-70.0, 10.0, -90.0, 60.0), crs=crs_latlon)
+        ax.set_extent((-70.0, 10.0, -28.0, 42.0), crs=crs_latlon)
         ax.stock_img()
         ax.coastlines(resolution='10m')
         ax.gridlines()
+        ax.set_xticks([-60, -40, -20, 0], crs=crs_latlon)
+        ax.set_yticks([-20, 0, 20, 40], crs=crs_latlon)
+        lon_formatter = LongitudeFormatter()
+        lat_formatter = LatitudeFormatter()
+        ax.xaxis.set_major_formatter(lon_formatter)
+        ax.yaxis.set_major_formatter(lat_formatter)
         # Add the date
         date_str = de.time.strftime('%d/%m/%Y %H:%M')
         props = dict(boxstyle='round', facecolor='white')
         ax.text(0.78, 0.02, date_str, transform=ax.transAxes, fontsize=14,
                 verticalalignment='top', bbox=props, fontfamily='monospace')
+        # Add the start and end points
+        for port in [['Lisbon', 38.725267, -9.150019], 
+                     ['Rio de Janeiro', -22.908333, -43.196389]]:
+            plt.plot(port[2], port[1], marker='.', markersize=5.0,
+                     markerfacecolor='red', markeredgecolor='red',
+                     transform=crs_latlon)
+            plt.text(port[2], port[1] - 3, port[0], horizontalalignment='center',
+                     color='black', transform=crs_latlon)
         # Add the vessel's position
         boat_colour = 'lime' if de.src == 'f' else 'black'
         plt.plot(de.lon, de.lat, marker='o', markersize=7.0, markeredgewidth=2.5,
@@ -84,6 +100,7 @@ def main():
         output_file = f'img{i:06}.png'
         plt.savefig(os.path.join(output_dir, output_file))
         plt.close()
+
 
 if __name__ == "__main__":
     main()
