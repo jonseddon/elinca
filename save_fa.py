d229e532fb1d5b136804e89c45e2c35c3e272f29
Jon Seddon <jon.seddon@metoffice.gov.uk>
2020-06-28 18:56:36 +0100

    Video saving!!!

--- save_fa.py
+++ save_fa.py
@@ -1,15 +1,16 @@
 """
-save_image.py
+save_fa.py
 from
 https://medium.com/@shintaroshiba/saving-3d-rendering-images-without-displays-on-python-opengl-f534a4638a0d
 """
-import time
 import cv2
 import numpy as np
 from OpenGL.GL import *
 import glfw
 
 from fieldanimation import FieldAnimation
+
+OUTPUT_FILE = 'image.avi'
 
 def main():
     DISPLAY_WIDTH = 800
@@ -28,6 +29,12 @@ def main():
     # Make the window's context current
     glfw.make_context_current(window)
 
+    # Setup the video writing
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    fps = 20
+    writer = cv2.VideoWriter(OUTPUT_FILE, fourcc, fps,
+                             (DISPLAY_WIDTH, DISPLAY_HEIGHT), True)
+
     # Set up the field animation
     field = np.load('/home/jseddon/python/fieldanimation/fieldanimation/'
                     'examples/wind_2016-11-20T00-00Z.npy')
@@ -36,17 +43,22 @@ def main():
     field_uv = np.flipud(np.dstack((U, -V)))
     fa = FieldAnimation(DISPLAY_WIDTH, DISPLAY_HEIGHT, field_uv, True, None)
 
-    glClear(GL_COLOR_BUFFER_BIT)
-    fa.draw()
-    glfw.swap_buffers(window)
+    n = 0
+    while n < 1200:
+        # TODO set frame rate on the OpenGL FieldAnimation side
+        n += 1
+        glClear(GL_COLOR_BUFFER_BIT)
+        fa.draw()
+        glfw.swap_buffers(window)
 
-    time.sleep(5)
-    image_buffer = glReadPixels(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT,
-                                OpenGL.GL.GL_RGB, OpenGL.GL.GL_UNSIGNED_BYTE)
-    image = np.frombuffer(image_buffer, dtype=np.uint8).reshape(DISPLAY_WIDTH,
-                                                                DISPLAY_HEIGHT,
-                                                                3)
-    cv2.imwrite("image.png", image)
+        image_buffer = glReadPixels(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT,
+                                    OpenGL.GL.GL_BGR, OpenGL.GL.GL_UNSIGNED_BYTE)
+        image = np.frombuffer(image_buffer, dtype=np.uint8).reshape(DISPLAY_WIDTH,
+                                                                    DISPLAY_HEIGHT,
+                                                                    3)
+        writer.write(np.flipud(image))
+
+    writer.release()
     glfw.destroy_window(window)
     glfw.terminate()
 
